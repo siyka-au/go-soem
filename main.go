4a38d0d17a883c73e4744d5224e763b7c7ed923c
piratecarrot <39475419+piratecarrot@users.noreply.github.com>
2022-01-18 16:55:45 +1100

    Implemented basic functions

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ package main
 #include <stdio.h>
 #include <stdlib.h>
 #include <soem/ethercat.h>
+
 */
 import "C"
 import (
+	"errors"
 	"fmt"
 	"unsafe"
 )
@@ -16,14 +18,21 @@ import (
 func main() {
 	master, err := NewSOEMMaster("enp6s0f0")
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	print(master)
+	master.ConfigInit()
+	fmt.Printf("Found %d attached slaves\n", master.SlaveCount)
 }
 
+type Context struct {
+}
+type Slave struct {
+}
 type SOEM struct {
-	Name string
-	Roll int
+	SlaveCount int
+	Slaves     []Slave
+	context    C.ecx_contextt
 }
 
 func NewSOEMMaster(ifname string) (*SOEM, error) {
@@ -31,13 +40,37 @@ func NewSOEMMaster(ifname string) (*SOEM, error) {
 	cifname := C.CString(ifname)
 	defer C.free(unsafe.Pointer(cifname))
 
-	if int(C.ec_init(cifname)) <= 0 {
-		return nil, fmt.Errorf("error opening interface ifname = %s", ifname)
+	soem.context = C.ecx_context
+
+	if int(C.ecx_init(&soem.context, cifname)) <= 0 {
+		return nil, fmt.Errorf("error opening interface %s", ifname)
 	}
 
 	return soem, nil
 }
 
-func (m SOEM) slaveCount() int {
-	return int(C.ec_slavecount())
+func (m *SOEM) ConfigInit() error {
+	m.SlaveCount = int(C.ecx_config_init(&m.context, 0))
+	if m.isError() {
+		return errors.New("error when initialising slaves")
+	}
+
+	return nil
+}
+
+func (m *SOEM) ConfigMap() error {
+	m.SlaveCount = int(C.ecx_config_init(&m.context, 0))
+	if m.isError() {
+		return errors.New("error when initialising slaves")
+	}
+
+	return nil
+}
+
+func (m *SOEM) getSlave(pos uint) (*Slave, error) {
+	return nil, nil
+}
+
+func (m *SOEM) isError() bool {
+	return int(C.ecx_iserror(&m.context)) > 0
 }
