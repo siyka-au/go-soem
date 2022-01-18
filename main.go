7c71e5b2f1b72a457c0e20816ad146d630da8475
piratecarrot <39475419+piratecarrot@users.noreply.github.com>
2022-01-18 17:19:54 +1100

    Starting to flesh out slave handling

--- main.go
+++ main.go
@@ -25,10 +25,35 @@ func main() {
 	fmt.Printf("Found %d attached slaves\n", master.SlaveCount)
 }
 
-type Context struct {
-}
 type Slave struct {
+	// Manufacturer from EEprom
+	VendorID uint32
+	// ID from EEprom
+	ProductCode uint32
+	// revision from EEprom
+	Revision uint32
+
+	Name string
+
+	// state of slave
+	State uint16
+	// AL status code
+	ALStatusCode uint16
+	// Configured address
+	ConfiguredAddress uint16
+	// Alias address
+	AliasAddress uint16
+
+	// Interface type
+	InterfaceType uint16
+	// Device type
+	DeviceType uint16
+	// output bits
+	OutputBits uint16
+	// output bytes, if Obits < 8 then Obytes = 0
+	OutputBytes uint32
 }
+
 type SOEM struct {
 	SlaveCount int
 	Slaves     []Slave
@@ -73,4 +98,18 @@ func (m *SOEM) getSlave(pos uint) (*Slave, error) {
 
 func (m *SOEM) isError() bool {
 	return int(C.ecx_iserror(&m.context)) > 0
+}
+
+func marshalSlave(cslave C.ec_slavet) *Slave {
+	slave := new(Slave)
+	slave.VendorID = uint32(cslave.eep_man)
+	slave.ProductCode = uint32(cslave.eep_id)
+	slave.Revision = uint32(cslave.eep_rev)
+	slave.Name = C.GoString(&cslave.name[0])
+
+	slave.State = uint16(cslave.state)
+	slave.ALStatusCode = uint16(cslave.ALstatuscode)
+	slave.AliasAddress = uint16(cslave.aliasadr)
+	slave.ConfiguredAddress = uint16(cslave.configadr)
+	return slave
 }
